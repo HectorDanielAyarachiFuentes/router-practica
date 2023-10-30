234b7554aa9efea3d849e1258c574a5d97a3a20e
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-10-30 17:47:57 -0300

    tfgh

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom'; // Importa createRoot desde react-dom
+import App from './App.jsx';
+import './index.css';
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root')); // Usa createRoot para crear el root
 root.render(
   <React.StrictMode>
     <BrowserRouter>

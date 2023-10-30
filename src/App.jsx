a9c5773b1821f717283f114ed778fb9ab7c53fce
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-10-30 17:39:06 -0300

    459

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Layout  from "./pages/Layout";
-import About  from "./pages/About";
-import Home  from "./pages/Home";
-import Default  from "./pages/Default";
-import Dashboard  from "./pages/Dashboard";
+import Layout from "./pages/Layout";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Default from "./pages/Default";
+import Dashboard from "./pages/Dashboard";
+
 function App() {
   return (
-    <div>
-      <h1>Routes</h1>
+    <div className="pokemon-app">
+      <h1 className="pokemon-title">Pokémon Routes</h1>
       <Routes>
         <Route path="/" element={<Layout />}>
-           <Route path="about" element={<About />} />
-           <Route path="/" element={<Home />} />
-           <Route path="dashboard" element={<Dashboard />} />
-           <Route path="*" element={<Default />} />
+          <Route path="about" element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="*" element={<Default />} />
         </Route>
       </Routes>
     </div>
